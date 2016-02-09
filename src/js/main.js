4d5d478347efb2891558735b86e9a27468e6248e
Sergiy Miroshnychenko <m1ron.com@gmail.com>
2016-02-09 15:32:40 +0700

    header & navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,42 @@
 /*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true, node: true */
 /*global window, console, document, $, jQuery, google */
 
-/**
- * On document ready
- */
+/** On document ready */
 $(document).ready(function () {
 
 	/** Fastclick */
 	FastClick.attach(document.body);
+
+
+	/** Header */
+	$('.header').each(function () {
+		var body = $('body'), time = 300;
+
+		function Handler(event) {
+			var target = $(event.target);
+			if (target.closest('.pure-menu').length === 0) {
+				Toggle(event);
+			}
+		}
+
+		function Toggle(e) {
+			if (body.hasClass('nav-open')) {
+				body.removeClass('nav-open').unbind('touchstart click', Handler);
+				setTimeout(function () {
+					body.removeClass('nav-pre');
+				}, time);
+			} else {
+				body.addClass('nav-pre');
+				setTimeout(function () {
+					body.addClass('nav-open').bind('touchstart click', Handler);
+				}, 10);
+			}
+			e.preventDefault();
+		}
+
+		$('.toggle', this).on('click', Toggle);
+	});
+
 
 	/** Fullpage */
 	$('.fullpage').each(function () {
@@ -29,5 +58,4 @@ $(document).ready(function () {
 		});
 		$.fn.fullpage.setAllowScrolling(false);
 	});
-
 });
